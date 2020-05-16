a3bdc4e9cebabd6ab78834864469ab4516ae8be7
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-05-15 23:06:33 -0500

    added UV index and 2nd API

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 //all variables
- const apiKey = "6b8e9a03868aa6b59b31dc84696dc422"
+ const apiKey = "6b8e9a03868aa6b59b31dc84696dc422";
+ const apiKey2 = '56149c9c2961bc5e9bc85376b22b499b';
+ let cityLat = navigator.geolocation;
+ let cityLon = navigator.geolocation;
 //all functions
 function weatherSearch(areaSearch) {
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${areaSearch},us&appid=${apiKey}&units=imperial`;
+    //let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${areaSearch},us&appid=${apiKey}&units=imperial`;
     $.ajax({
-        url: queryURL,
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${areaSearch},us&appid=6b8e9a03868aa6b59b31dc84696dc422&units=imperial`,
         method: "GET"
     }).then(function (response) {
+        console.log(response)
         var queryURL =
+        $('.cityName').text(response.name);
+        $('.temp').text(`Temperature: ${response.main.temp}`);
+        $('.humidity').text(`Humidity:  ${response.main.humidity}`);
+        $('.wind-speed').text(`Wind Speed: ${response.wind.speed}`);
+        //
+    });
+    //let queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude={part}&appid=${apiKey2}`;
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/onecall?lat=29.7604&lon=-95.3698&exclude=minutely&appid=6b8e9a03868aa6b59b31dc84696dc422`,
+        method: "GET"
+    }).then(function (response) {
         console.log(response);
-        //step 1 - create element
-        let tempEl = $('<p>');
-        let humidEl = $('<p>');
-        let windSpeed = $('<p>');
-        let uvIndex = $('<p>');
-        //step 2 - giving this attribute its values, text contentent, atrributes..
-        tempEl.text(`Temperature:  ${response.main.temp}`);
-        humidEl.text(`Humidity:  ${response.main.humidity}`);
-        windSpeed = (`Wind Speed: ${response.wind.speed}`);
-        //uvIndex = (`UV Index:  ${response.wind.speed}`);
-        //step 3 - render it on the page
-        $('#oneDay').append(tempEl);
-        $('#oneDay').append(humidEl);
-        $('#oneDay').append(windSpeed);
-    })
+        $('.uv-index').text(`UV Index: ${response.current.uvi}`); 
+        
     // weather search logic here
+    });
 }
-
 //all on click events below:
 $("#searchBtn").click(function(){
-    var citySearch = $("#searchQueery").val();
+    var citySearch = $("#searchQuery").val();
     weatherSearch(citySearch);
+
 
 });
 
@@ -39,5 +42,4 @@ $('#history').click(function(){
     var citySearch = $(this).val();
     weatherSearch(citySearch);
 
-})
-*/
+})*/
